728d51e52076987e8b86e7b85aa84705efcbf8d5
franco <nahuelgodina@gmail.com>
2022-07-14 15:31:43 -0300

    Set up Projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,141 @@
+import "./Projects.css";
+import { Icon } from '@iconify/react';
+import pcbuilder from "../images/pcbuilder.png";
+import shoppingcart from "../images/shoppingcart.png";
+import memorygame from "../images/memorygame.png";
+import restaurantpage from "../images/restaurantpage.png";
+import etchasketch from "../images/etchasketch.png";
+import weatherapp from "../images/weatherapp.png"
+
 function Projects() {
     return(
-        <div></div>
+        <div className="main-container">
+            <div>
+            <h1>My <span className="text-red">Projects</span></h1>
+            </div>
+            <div className="project-container">
+                <div className="project">
+                    <img src={pcbuilder} alt="Main page of the project" className="project-image"></img>
+                    <div className="information">
+                        <section className="title"> Express pc builder</section>
+                        <section className="description">
+                            Inventory App that includes categories and items. Each of these include all of the CRUD Methods, so anybody that's visiting the site can Create, Read, Update or Delete any items or category.
+                            <br /><br />
+                            Back-End: Node.js/Express 
+                            <br />
+                            Database: MongoDB
+                            <br />
+                            Front-End: HTML, Bootstrap 5, Pug.
+                        </section>
+                        <section className="project-button">
+                        <a className="livedemo" href="https://infinite-temple-59082.herokuapp.com/catalog">
+                            Live demo <Icon icon="bi:laptop" inline={true} />
+                        </a>
+                        <a className="code" href="https://github.com/FrancoGodina/express-pc-builder">
+                            Code <Icon icon="akar-icons:github-fill" inline={true} />
+                        </a>
+                        </section>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={shoppingcart} alt="Main page of the project" className="project-image"></img>
+                    <div className="information">
+                        <section className="title"> Shopping cart</section>
+                        <section className="description">
+                            A simple shopping cart application. It allows users to add items to the cart and then remove or add more of each from the cart's section.
+                            <br /><br />
+                            Front-End: HTML, CSS, JavaScript, React.js.
+                        </section>
+                        <section className="project-button">
+                        <a className="livedemo" href="https://francogodina.github.io/shopping-cart/">
+                            Live demo <Icon icon="bi:laptop" inline={true} />
+                        </a>
+                        <a className="code" href="https://github.com/FrancoGodina/shopping-cart">
+                            Code <Icon icon="akar-icons:github-fill" inline={true} />
+                        </a>
+                        </section>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={memorygame} alt="Main page of the project" className="project-image"></img>
+                    <div className="information">
+                        <section className="title">Memory game</section>
+                        <section className="description">
+                            Memory game created with React. You'll be presented with multiple images. The images will shuffle every-time they are clicked. DO NOT click any images twice, else the score will reset to zero. The goal is to get the highest score possible.
+                            <br /><br />
+                            Front-End: HTML, CSS, JavaScript, React.js.
+                        </section>
+                        <section className="project-button">
+                        <a className="livedemo" href="https://francogodina.github.io/memory-game/">
+                            Live demo <Icon icon="bi:laptop" inline={true} />
+                        </a>
+                        <a className="code" href="https://github.com/FrancoGodina/memory-game">
+                            Code <Icon icon="akar-icons:github-fill" inline={true} />
+                        </a>
+                        </section>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={weatherapp} alt="Main page of the project" className="project-image"></img>
+                    <div className="information">
+                        <section className="title">Weather App</section>
+                        <section className="description">
+                            A weather app that shows current weather for a city. It uses OpenWeatherMap API to fetch current weather.
+                            <br /><br />
+                            Back-end: OpenWeatherMapAPI.
+                            <br />
+                            Front-End: HTML, CSS, JavaScript.
+                        </section>
+                        <section className="project-button">
+                        <a className="livedemo" href="https://francogodina.github.io/Weather-App/">
+                            Live demo <Icon icon="bi:laptop" inline={true} />
+                        </a>
+                        <a className="code" href="https://github.com/FrancoGodina/Weather-App">
+                            Code <Icon icon="akar-icons:github-fill" inline={true} />
+                        </a>
+                        </section>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={restaurantpage} alt="Main page of the project" className="project-image"></img>
+                    <div className="information">
+                        <section className="title"> Restaurant page</section>
+                        <section className="description">
+                            Restaurant page made with Webpack. It contains various sections, each with different information.
+                            <br /><br />
+                            Front-End: HTML, CSS, JavaScript, Webpack.
+                        </section>
+                        <section className="project-button">
+                        <a className="livedemo" href="https://francogodina.github.io/Restaurant-Page/">
+                            Live demo <Icon icon="bi:laptop" inline={true} />
+                        </a>
+                        <a className="code" href="https://github.com/FrancoGodina/Restaurant-Page">
+                            Code <Icon icon="akar-icons:github-fill" inline={true} />
+                        </a>
+                        </section>
+                    </div>
+                </div>
+                <div className="project">
+                    <img src={etchasketch} alt="Main page of the project" className="project-image"></img>
+                    <div className="information">
+                        <section className="title">Etch-a-Sketch</section>
+                        <section className="description">
+                            Inventory App that includes categories and items. Each of these include all of the CRUD Methods, so anybody that's visiting the site can Create, Read, Update or Delete any items or category.
+                            <br /><br />
+                            Front-End: HTML, CSS, JavaScript.
+                        </section>
+                        <section className="project-button">
+                        <a className="livedemo" href="https://francogodina.github.io/Etch-a-Sketch/">
+                            Live demo <Icon icon="bi:laptop" inline={true} />
+                        </a>
+                        <a className="code" href="https://github.com/FrancoGodina/Etch-a-Sketch">
+                            Code <Icon icon="akar-icons:github-fill" inline={true} />
+                        </a>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
