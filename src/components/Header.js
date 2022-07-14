b6c02d0d6d5e95454653c3fe0d2fff17bd89cf85
franco <nahuelgodina@gmail.com>
2022-07-13 22:14:48 -0300

    Set up Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,29 @@
+import { Link } from "react-router-dom"
+import "./Header.css"
+
 function Header() {
     return(
-        <div></div>
+        <ul>
+            <div>
+                <li>
+                    <Link className="header-name" to="/">
+                        Franco Godina
+                    </Link>
+                </li>
+            </div>
+            <div>
+                <li>
+                        <Link className="header-right" to="/about">
+                            About
+                        </Link>
+                </li>
+                <li>
+                    <Link className="header-right" to="/projects">
+                        Projects
+                    </Link>
+                </li>
+            </div>
+        </ul>
     )
 }
 
