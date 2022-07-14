622e9b475ad9e72f272f907d6f91f1c5f6523e42
franco <nahuelgodina@gmail.com>
2022-07-14 11:31:11 -0300

    Set up home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
+import "./Home.css"
+
 function Home() {
     return(
         <div className="home">
-            <h1>Hello world</h1>
+            <h1>I'm <span className="home-name">Franco Godina</span></h1>
+            <h1>An Aspiring Web Developer</h1>
+            <div className="home-buttons">
+                <Link className="home-about" to="/about">
+                    About me
+                </Link>
+                <Link className="home-projects" to="/projects">
+                    Projects
+                </Link>
+            </div>
         </div>
     )
 }
